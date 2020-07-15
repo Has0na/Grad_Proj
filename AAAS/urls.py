c13575b6d311fe75518e414651dee2e4d133ab72
Has0na <git@github.com>
2020-07-15 02:39:37 +0200

    Done

--- AAAS/urls.py
+++ AAAS/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.signIn, name="$"),
