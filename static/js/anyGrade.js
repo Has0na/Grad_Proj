34b6aedc263212a187bb36e26512eeb84e6f8be9
Has0na <git@github.com>
2020-07-16 10:16:58 +0200

    Done

--- static/js/anyGrade.js
+++
@@ -1,7 +0,0 @@
-var data = JSON.parse("{{data|escapejs}}");
-	var dataNode = document.getElementById('alldata');
-	dataNode.innerHTML+="{{data|escapejs}}";
-	dataNode = document.getElementById('neatdata');
-	for(var x in data){
-		dataNode.innerHTML+=x+' : '+data[x]+'<br><br>';
-	}
